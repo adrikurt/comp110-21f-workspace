dcfc707c5dc7f7683d88b74e60a25369e5850792
Adriana Kurtovic <adrikurt@email.unc.edu>
2021-08-29 19:49:14 -0400

    Final formatting edits for autograder.

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -2,7 +2,7 @@
 
 __author__ = "730395502"
 
-name: str = input ("Who are you? ")
-print ("Wow, " + name + ", nice to meet you!")
-print (name + ", you are the most awesome person to every exist.")
-print ("I hope you have the most amazing day ever, " + name + ".")
+name: str = input("Who are you? ")
+print("Wow, " + name + ", nice to meet you!")
+print(name + ", you are the most awesome person to every exist.")
+print("I hope you have the most amazing day ever, " + name + ".")
