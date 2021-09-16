32e255418c1ed9fb6c8b1b9dabafd1cbc7388069
Adriana Kurtovic <adrikurt@email.unc.edu>
2021-09-15 22:11:49 -0400

    Finished find duplicates

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,20 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730395502"
+
+
+word: str = input("Enter a word: ")
+i: int = 0
+j: int = 0
+occurance = 1
+while i < len(word) - 1:
+    j += 1
+    while j < len(word) - 1:
+        if word[i] == word[j+1]:
+            occurance = occurance + 1
+        j += 1
+    i += 1
+if occurance > 1:
+    print("Found duplicate: True")
+else:
+    print("Found duplicate: False")
