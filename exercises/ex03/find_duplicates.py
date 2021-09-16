be07799558ef38fa292725128067be1cb4e90b1a
Adriana Kurtovic <adrikurt@email.unc.edu>
2021-09-15 22:48:10 -0400

    Ex03 Submission 2

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -5,16 +5,14 @@ __author__ = "730395502"
 
 word: str = input("Enter a word: ")
 i: int = 0
-j: int = 0
-occurance = 1
-while i < len(word) - 1:
-    j += 1
-    while j < len(word) - 1:
-        if word[i] == word[j+1]:
-            occurance = occurance + 1
+j: int = i + 1
+dup: bool = False
+while i < len(word):
+    char = word[i]
+    j: int = i + 1
+    while j < len(word):
+        if char == word[j]:
+            dup = bool = True
         j += 1
     i += 1
-if occurance > 1:
-    print("Found duplicate: True")
-else:
-    print("Found duplicate: False")
+print("Found duplicate: " +str(dup))
--- exercises/ex03/tar_heels.py
+++ exercises/ex03/tar_heels.py
@@ -6,7 +6,7 @@ __author__ = "730395502"
 integer: str = input("Enter an int: ")
 int_integer = int(integer)
 if (int_integer % 2 == 0 and int_integer % 7 == 0):
-    print ("TAR HEELS")
+    print("TAR HEELS")
 else:
     if (int_integer % 2 == 0):
         print("TAR")
